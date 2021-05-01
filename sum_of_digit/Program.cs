4e8b5e92fe7f7a7a774311f69b706841f6c9e99f
Rahul <rahulg3550@gmail.com>
2021-05-01 16:13:32 +0530

    sum of digit

---
+++ sum_of_digit/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace sum_of_digit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number,sum=0,r;
+            Console.WriteLine("Enter the number");
+            number=int.Parse(Console.ReadLine());
+            while(number>0)
+            {
+                r=number%10;
+                sum=sum+r;
+                number/=10;
+            }
+            System.Console.WriteLine($"sum is {sum}");
+        }
+    }
+}
