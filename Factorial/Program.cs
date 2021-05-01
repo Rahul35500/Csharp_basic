08580717f35e50f5ba9147e3187e1c3e89874625
Rahul <rahulg3550@gmail.com>
2021-05-01 16:08:13 +0530

    Factorial

---
+++ Factorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, Factorial = 1;
+            System.Console.WriteLine("Enter the number");
+            number = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= number; i++)
+            {
+                Factorial *= i;
+            }
+            System.Console.WriteLine($"Factorial of {number} is {Factorial}");
+        }
+    }
+}
