0b7067239a560e46be70be96ab71928292d2aa2f
Rahul <rahulg3550@gmail.com>
2021-05-01 16:05:10 +0530

    Armstrong

---
+++ Armstrong/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Armstrong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, temp, sum = 0, r;
+            Console.WriteLine("Enter the number");
+            number = int.Parse(Console.ReadLine());
+            temp = number;
+            while (number > 0)
+            {
+                r = number % 10;
+                sum = sum + (r * r * r);
+                number /= 10;
+            }
+            if (temp == sum)
+            {
+                System.Console.WriteLine("number is palindrome");
+            }
+            else
+            {
+                System.Console.WriteLine("number is not palindrome");
+            }
+        }
+    }
+}
