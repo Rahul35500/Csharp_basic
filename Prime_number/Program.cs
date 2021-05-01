c7ba456c973963fee0ea2afacd481478c76baa57
Rahul <rahulg3550@gmail.com>
2021-05-01 16:11:55 +0530

    prime number

---
+++ Prime_number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Prime_number
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int number, flag = 0;
+            Console.WriteLine("Enter the number to check prime or not ");
+            number = int.Parse(Console.ReadLine());
+            if (number % 2 != 0)
+            {
+                Console.WriteLine("Number is prime ");
+                flag = 1;
+            }
+            if (flag == 0)
+            {
+                Console.WriteLine("number is not prime");
+            }
+        }
+    }
+}
