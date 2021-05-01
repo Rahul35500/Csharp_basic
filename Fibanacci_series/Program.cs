fc027fe63a71e4f54587e0eff6b992bf32784166
Rahul <rahulg3550@gmail.com>
2021-05-01 16:09:32 +0530

    Fibonacci series

---
+++ Fibanacci_series/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Fibanacci_series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, n3, number;
+            n1 = 0;
+            n2 = 1;
+            Console.WriteLine("Enter the number of elements");
+            number = int.Parse(Console.ReadLine());
+            System.Console.Write($"{n1} {n2} ");
+            for (int i = 2; i < number; i++)
+            {
+                n3 = n1 + n2;
+                n1 = n2;
+                n2 = n3;
+                System.Console.Write($"{n3} ");
+
+            }
+
+        }
+    }
+}
