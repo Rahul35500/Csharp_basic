54c50dc456c3c039ac06e50ce3d1fe7326ab7867
Rahul <rahulg3550@gmail.com>
2021-05-01 16:10:31 +0530

    palindrome

---
+++ Palindrome/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, temp, sum=0, hold_reverse;
+            System.Console.WriteLine("Enter the number");
+            number = int.Parse(Console.ReadLine());
+            temp = number;
+            while (number > 0)
+            {
+                hold_reverse = number % 10;
+                sum = (sum * 10) + hold_reverse;
+                number = number / 10;
+            }
+            if (sum == temp)
+            {
+                System.Console.WriteLine("number is palindrome");
+            }
+            else
+            {
+                System.Console.WriteLine("number is not palindrome");
+            }
+
+        }
+    }
+}
