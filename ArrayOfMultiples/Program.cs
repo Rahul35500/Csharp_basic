a46bcc5e4c2d5c90b8355e93e40bf2214000de9f
Rahul <rahulg3550@gmail.com>
2021-05-18 17:12:15 +0530

    array of multiples

---
+++ ArrayOfMultiples/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ArrayOfMultiples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Enter the number which you want in multiples");
+            int number;
+            int.TryParse(Console.ReadLine(), out number);
+            System.Console.WriteLine("Enter the length of multiples");
+            int length;
+            int.TryParse(Console.ReadLine(), out length);
+            for (int i = 0; i < ArrayMultiples(number, length).Length; i++)
+            {
+                System.Console.Write($"{ArrayMultiples(number, length)[i]} {" "}");
+            }
+        }
+        public static int[] ArrayMultiples(int number, int length)
+        {
+            int[] arraySecond = new int[length];
+            int j = 1;
+            for (int i = 0; i < length; i++)
+            {
+                arraySecond[i] = number * j;
+                j++;
+            }
+            return arraySecond;
+        }
+    }
+
+}
