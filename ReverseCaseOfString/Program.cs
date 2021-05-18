afca31f7bb17dbb704b79deaf43862f553e90f95
Rahul <rahulg3550@gmail.com>
2021-05-18 17:17:55 +0530

    Reverse case of string

---
+++ ReverseCaseOfString/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ReverseCaseOfString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test=ReverseString("ReVerSeSTriNG");
+            System.Console.WriteLine(test);
+            System.Console.WriteLine(ReverseString("heLLaAw wold"));
+        }
+        static string ReverseString(string name)
+        {
+            char[] ReverseCharacter=new char[name.Length];
+            for(int i=0;i<name.Length;i++)
+            {
+                if(name[i]>=65 && name[i]<=90)
+                {
+                    ReverseCharacter[i]=(char)(name[i]+32);
+                }
+                else if(name[i]>=97 && name[i]<=122)
+                {
+                    ReverseCharacter[i]=(char)(name[i]-32);
+                }
+                else
+                {
+                    ReverseCharacter[i]=name[i];
+                }
+            }
+            string name1=new string(ReverseCharacter);
+            return name1;
+
+        }
+    }
+}
