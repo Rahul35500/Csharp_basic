d86563832dec97d442aa6804c346d196fbb54563
Rahul <rahulg3550@gmail.com>
2021-05-18 17:15:54 +0530

    convert number to corresponding month name

---
+++ ConvertNumberToCorrespondingMonthName/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConvertNumberToCorrespondingMonthName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Enter the number between one and thirteen");
+            int number;
+            int.TryParse(Console.ReadLine(),out number);
+            System.Console.Write($"{number} number in month = ");
+            System.Console.WriteLine(Getname(number));
+
+
+        }
+        static string Getname(int month)
+        {
+            DateTime Month=new DateTime(2020,month,1);
+            return Month.ToString("MMMM");
+        }
+    }
+}
